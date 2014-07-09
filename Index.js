68f113da758620a1c29dcda1b6a1aeaefad43e64
FuryFlower <hwebb@thoughtworks.com>
2014-07-09 19:35:41 -0400

    first commit

---
+++ Index.js
@@ -0,0 +1,8 @@
+function addition(){
+
+	var x = parseInt(document.getElementById("num_1").value) 
+	var y = parseInt(document.getElementById("num_2").value)
+	var answer = x + y
+	alert(answer)
+
+}
