105d0f7690d166dec2e123880fba0fdd86c55a88
FuryFlower <hwebb@thoughtworks.com>
2014-07-09 20:24:41 -0400

    adds multiplcation and divison

--- Index.js
+++ Index.js
@@ -1,8 +1,29 @@
-function addition(){
+function multiply(){
 
-	var x = parseInt(document.getElementById("num_1").value) 
-	var y = parseInt(document.getElementById("num_2").value)
-	var answer = x + y
-	alert(answer)
+	var x = parseInt(document.getElementById("num_1").value); 
+	var y = parseInt(document.getElementById("num_2").value);
+	var answer = x * y;
+	alert(answer); 
 
 }
+
+function add(){
+
+	var x = parseInt(document.getElementById("num_1").value); 
+	var y = parseInt(document.getElementById("num_2").value);
+	var answer = x + y;
+	alert(answer); 
+
+}
+
+
+function divide(){
+
+	var x = parseInt(document.getElementById("num_1").value); 
+	var y = parseInt(document.getElementById("num_2").value);
+	var answer = x / y;
+	alert(answer); 
+
+}
+
+	
