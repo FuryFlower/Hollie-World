db7c8d66a5da0831ff114763cb33368559c4b1c9
FuryFlower <hwebb@thoughtworks.com>
2014-07-09 20:48:51 -0400

    changes dropdown textbox

--- Index.js
+++ Index.js
@@ -10,7 +10,9 @@ function multiply(){
 function add(){
 
 	var x = parseInt(document.getElementById("num_1").value); 
+	console.log (x)
 	var y = parseInt(document.getElementById("num_2").value);
+	console.log(y)
 	var answer = x + y;
 	alert(answer); 
 
